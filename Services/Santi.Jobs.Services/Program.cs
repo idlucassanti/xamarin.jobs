6ef392966efe07c8448c85303f5bbfe54f95b44a
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2022-06-19 05:47:04 -0300

    [001] - Criação dos projetos

---
+++ Services/Santi.Jobs.Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Santi.Jobs.Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
